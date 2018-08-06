b187450eece283103719fd51ba0482f786886e9a
wujiescl <17601517400@139.com>
2018-08-06 10:22:13 +0800

    数据加载进度框

---
+++ app/src/main/java/com/wujie/mylistview/Dialog/LodingActivity.java
@@ -0,0 +1,13 @@
+package com.wujie.mylistview.Dialog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LodingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loding);
+    }
+}
